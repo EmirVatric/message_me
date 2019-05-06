da63f23ae9b92c9eff5d7199a0aef5bbc0ce5a54
EmirVatric <vatricemir@gmail.com>
2019-05-06 02:04:09 +0200

    login and sing up template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,23 @@
 $(document).on('turbolinks:load', function () {
     $('.ui.dropdown')
         .dropdown();
-})
+});
+
+$(document).on('turbolinks:load', function () {
+
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('containera');
+
+    console.log(container.classList);
+
+    signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+        console.log(container.classList);
+    });
+
+    signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+        console.log(container.classList);
+    });
+});
